564d8126f7ec0e51ad95d8facc5247d596177b8c
Adrian Dąbek <adrian950113@gmail.com>
2023-06-23 13:49:06 +0200

    add formatting and output

--- task.js
+++ task.js
@@ -9,7 +9,7 @@ class JiraBoardController {
 
   async fetchIssues(jql) {
     const encodedJql = encodeURI(jql);
-    const response = await fetch(`${this.#baseUrl}/search?jql=${jql}`);
+    const response = await fetch(`${this.#baseUrl}/search?jql=${encodedJql}`);
     const body = await response.json();
     return body.issues;
   }
@@ -25,16 +25,10 @@ class IssuesService {
   async fetchIssuesSummaryOfComponentsWithoutLead() {
     const componentNames = await this.#fetchComponentNamesWithoutLead();
     const issues = await this.#fetchIssuesOfComponents(componentNames);
-
-    const issuesPerComponent = {};
-    componentNames.forEach((name) => (issuesPerComponent[name] = []));
-    issues.forEach((issue) => {
-      const issueName = issue.fields.summary;
-      issue.fields.components.forEach((component) => {
-        if (!issuesPerComponent[component.name]) return;
-        issuesPerComponent[component.name].push(issueName);
-      });
-    });
+    const issuesPerComponent = this.#groupIssuesByComponent(
+      issues,
+      componentNames
+    );
     return issuesPerComponent;
   }
 
@@ -55,9 +49,60 @@ class IssuesService {
     )})`;
     return this.taskBoardController.fetchIssues(query);
   }
+
+  #groupIssuesByComponent(issues, componentNames) {
+    const issuesPerComponent = componentNames.reduce(
+      (acc, name) => ({ ...acc, [name]: [] }),
+      {}
+    );
+    issues.forEach((issue) => {
+      const { summary, components } = issue.fields;
+      components
+        .filter((component) => issuesPerComponent[component.name])
+        .forEach((component) =>
+          issuesPerComponent[component.name].push(summary)
+        );
+    });
+    return issuesPerComponent;
+  }
+}
+
+class HumanFriendlyFormatter {
+  #makeBulletPoints(items) {
+    return items.map((item) => `- ${item}.`).join("\n");
+  }
+
+  formatRecordOfArrays(record) {
+    return Object.entries(record)
+      .map(([header, items]) => {
+        return `${header}:\n${this.#makeBulletPoints(items)}`;
+      })
+      .join("\n");
+  }
 }
 
 const controller = new JiraBoardController();
 const service = new IssuesService(controller);
+const formatter = new HumanFriendlyFormatter();
 
-service.fetchIssuesSummaryOfComponentsWithoutLead().then(console.log);
+/**
+ * Outputs
+ *
+ * Backend:
+ * -  Disable self-mentioning notifications.
+ * Synchronization:
+ * -  Sync the status of new issue created from a checklist item with that item's status.
+ * -  Create CFJS-to-IC funtionalities comparison table in user documentation.
+ * Templates:
+ * -  Global templates context - ON for all project and issue types by default.
+ * -  Templates do not load under Project Pages.
+ * -  Apply template according to label added to the issue.
+ * -  Edit template UI improvements.
+ * -  Add support for renaming statuses to Templates with non-default statuses.
+ */
+service
+  .fetchIssuesSummaryOfComponentsWithoutLead()
+  .then((issuesPerComponent) =>
+    formatter.formatRecordOfArrays(issuesPerComponent)
+  )
+  .then(console.log);
