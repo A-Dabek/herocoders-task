0f179079740beef8f38715e41c20cd0e530c8766
Adrian Dąbek <adrian950113@gmail.com>
2023-06-23 13:27:13 +0200

    organize code into layers

--- task.js
+++ task.js
@@ -1,31 +1,63 @@
-async function task() {
-  const response = await fetch(
-    "https://herocoders.atlassian.net/rest/api/3/project/SP/components"
-  );
-  const body = await response.json();
-  const componentsWithoutLead = body.filter((component) => !component.lead);
-  const componentNames = componentsWithoutLead.map(
-    (component) => component.name
-  );
+class JiraBoardController {
+  #baseUrl = "https://herocoders.atlassian.net/rest/api/3";
 
-  const jql = `project = SP AND component in (${componentNames.join(", ")})`;
-  const encodedJql = encodeURI(jql);
+  async fetchComponents() {
+    const response = await fetch(`${this.#baseUrl}/project/SP/components`);
+    const components = await response.json();
+    return components;
+  }
 
-  const issuesResponse = await fetch(
-    `https://herocoders.atlassian.net/rest/api/3/search?jql=${jql}`
-  );
-  const issuesBody = await issuesResponse.json();
-  const issues = issuesBody.issues;
-  const issuesPerComponent = {};
-  componentNames.forEach((name) => (issuesPerComponent[name] = []));
-  issues.forEach((issue) => {
-    const issueName = issue.fields.summary;
-    issue.fields.components.forEach((component) => {
-      if (!issuesPerComponent[component.name]) return;
-      issuesPerComponent[component.name].push(issueName);
-    });
-  });
-  return issuesPerComponent;
+  async fetchIssues(jql) {
+    const encodedJql = encodeURI(jql);
+    const response = await fetch(`${this.#baseUrl}/search?jql=${jql}`);
+    const body = await response.json();
+    return body.issues;
+  }
 }
 
-task().then(console.log);
+class IssuesService {
+  taskBoardController;
+
+  constructor(taskBoardController) {
+    this.taskBoardController = taskBoardController;
+  }
+
+  async fetchIssuesSummaryOfComponentsWithoutLead() {
+    const componentNames = await this.#fetchComponentNamesWithoutLead();
+    const issues = await this.#fetchIssuesOfComponents(componentNames);
+
+    const issuesPerComponent = {};
+    componentNames.forEach((name) => (issuesPerComponent[name] = []));
+    issues.forEach((issue) => {
+      const issueName = issue.fields.summary;
+      issue.fields.components.forEach((component) => {
+        if (!issuesPerComponent[component.name]) return;
+        issuesPerComponent[component.name].push(issueName);
+      });
+    });
+    return issuesPerComponent;
+  }
+
+  async #fetchComponentNamesWithoutLead() {
+    const components = await this.taskBoardController.fetchComponents();
+    const componentsWithoutLead = components.filter(
+      (component) => !component.lead
+    );
+    const componentNames = componentsWithoutLead.map(
+      (component) => component.name
+    );
+    return componentNames;
+  }
+
+  async #fetchIssuesOfComponents(componentNames) {
+    const query = `project = SP AND component in (${componentNames.join(
+      ", "
+    )})`;
+    return this.taskBoardController.fetchIssues(query);
+  }
+}
+
+const controller = new JiraBoardController();
+const service = new IssuesService(controller);
+
+service.fetchIssuesSummaryOfComponentsWithoutLead().then(console.log);
